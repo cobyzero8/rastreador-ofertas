5a436b7ba505c38f285d234a1e3044a49da14720
cobyzero8 <cobyzero8@hotmail.com>
2026-08-08 20:52:14 -0500

    Update conecta_retail.py

--- scrapers/conecta_retail.py
+++ scrapers/conecta_retail.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from utils import sanitizar_url, limpiar_precio_pnp
 
-def motor_conecta_retail(url, limite, headers, tag):
+def motor_conecta_retail(url, limite=999999.0, headers=None):
     productos = []
     url = sanitizar_url(url)
     try:
